a42018daf62b275fb5d00dd6de4b5fbd019a4d80
Olga Karpenko <okarpenko@users.noreply.github.com>
2026-09-15 00:57:44 -0700

    Added Mockito examples.

---
+++ src/main/java/testing/mockito/UserService.java
@@ -0,0 +1,15 @@
+package testing.mockito;
+
+public class UserService {
+    private final UserRepository repository;
+
+    public UserService(UserRepository repository) {
+        this.repository = repository;
+    }
+
+    public String getUserGreeting(int id) {
+        String name = repository.findNameById(id);
+        return "Hello, " + name;
+       // return "Hello, Alice";
+    }
+}
