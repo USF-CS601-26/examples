3fe2119b3fe4812eccae5a75f8b6368b2bbe7e74
Olga Karpenko <okarpenko@users.noreply.github.com>
2026-09-15 08:47:15 -0700

    Added inheritance examples.

---
+++ src/main/java/inheritance/staticmethods/OverridingExample.java
@@ -0,0 +1,20 @@
+package inheritance.staticmethods;
+
+/** The example demonstrates that static methods do hot behave polymorphically. */
+public class OverridingExample {
+    public static void main(String[] args) {
+        Superclass sub = new Subclass();
+        sub.func1(); // child's version
+        sub.staticFunc1();
+
+        // Both will call the parent's staticFunc1
+        //Superclass.staticFunc1();
+        //sub.staticFunc1(); // Calls parent's staticFunc1 because the compiler looks at the type of the variable sub
+
+        // Which function will be invoked?
+
+        //Subclass.staticFunc1();
+
+
+    }
+}
